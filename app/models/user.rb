a5d968105f1d91282c86e2a1b53486287b4cdbf3
Robin van de Griend <robinvdgriend@outlook.com>
2015-12-11 16:56:24 +0100

    Add User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  validates :name, presence: true, format: { with: /\A\S*\Z/ }, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "ExampleUser")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "name should not contain whitespace" do
+    @user.name = "foo bar"
+    assert_not @user.valid?
+  end
+
+  test "name should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
