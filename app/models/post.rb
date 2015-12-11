2fcced59de9a21269fe67b8cd4616619a5463197
Robin van de Griend <robinvdgriend@outlook.com>
2015-12-11 19:44:24 +0100

    Add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
+require 'uri'
+
 class Post < ActiveRecord::Base
+  # TODO: automatic link formatting ("www.test.com" to "http://test.com")
+
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 140 }
+  validates :link, presence: true, format: { with: URI::regexp }
 end
